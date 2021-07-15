97bccb4b12cd6c3ffe83bfba821133920d660b8f
Mohit Bajaj <mohit_b35@hotmail.com>
2021-07-15 16:12:32 +0530

    v5.4 - Add login functionality - Create login form, visit this via the /login GET route - Add POST /login route - In POST /login, passport.authenticate middleware to authenticate user - if successful, move to next callback (i.e. route handler callback)

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const User = require('../models/user');
 
 // const catchAsync = require('../utils/catchAsync');
@@ -12,16 +13,29 @@ router.get('/register', (req, res) => {
 router.post('/register', async (req, res) => {
 	// Note - not wrapping the function in catchAsync, as we do not want the default Error handler to handle an error here and show the error template. Instead we want to stay on this page. So we use a try catch block within this function itself.
 	try {
-		const { username, email, password } = req.body.user;
+		const { username, email, password } = req.body;
 		const user = new User({ username, email });
 	
 		const savedUser = await User.register(user, password);
-		req.flash('success', 'Welcome to Yelpcamp');
+		req.flash('success', 'Welcome to Yelpcamp!');
 		res.redirect('/campgrounds');
 	} catch (error) {
 		req.flash('error', error.message);
 		res.redirect('/register');
 	}
 });
+
+router.get('/login', (req, res) => {
+	res.render('users/login');
+});
+
+
+router.post('/login',
+	passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),
+	async (req, res) => {
+		req.flash('success', 'Welcome back!')
+		res.redirect('/campgrounds');
+	}
+);
 
 module.exports = router;
